ad3f888acc17d3f8a2765159232e22c1b3622bdc
Jonathan Meza <jonathanmeza@ticsocial.com.co>
2025-02-20 09:46:03 -0500

    [CREATE] serializer validations

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+
+ # Validator
+def name_length(value):
+    if len(value) <= 3:
+        raise serializers.ValidationError("Name is too short")
+    else:
+        return value     
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -18,3 +26,18 @@ class MovieSerializer(serializers.Serializer):
         instance.save()
         return instance
     
+    # Object-level Validation
+    def validate(self, data):
+        if data["name"] == data["description"]:
+            raise serializers.ValidationError("name and description should be different")
+        else:
+            return data
+
+    # Field-level Validation
+    def validate_description(self, value):
+        if len(value) <= 2:
+            raise serializers.ValidationError("Description is too short")
+        else:
+            return value
+
+        
