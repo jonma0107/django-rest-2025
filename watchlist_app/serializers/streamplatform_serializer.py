837062f31210bd413a6d4107def93064ce001131
Jonathan Meza <jduplantier44@gmail.com>
2025-03-07 00:32:28 -0500

    [update] Nested Serializers

--- watchlist_app/serializers/streamplatform_serializer.py
+++ watchlist_app/serializers/streamplatform_serializer.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from watchlist_app.models.watchmate import StreamPlatform
+from watchlist_app.serializers.watchlist_serializer import WatchListSerializer
 
 
 class StreamPlatformSerializer(serializers.ModelSerializer):
+
+    watchlist = WatchListSerializer(many = True, read_only = True)
     
     class Meta:
         model = StreamPlatform
